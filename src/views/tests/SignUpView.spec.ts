bab61524958498b297ee7bea936ae6954c907202
adrianmedeirosdev <lima.adrian13@gmail.com>
2024-08-22 21:57:54 -0300

    test: add render views unit tests

---
+++ src/views/tests/SignUpView.spec.ts
@@ -0,0 +1,13 @@
+import {describe, expect, it} from 'vitest'
+import { render, screen } from '@testing-library/vue'
+import SignUpView from '../SignUpView.vue'
+import router from '../../router'
+
+
+describe('SignUpView', () => {
+    it('should render', () => {
+        render(SignUpView, {global: { plugins: [router]} })
+        const component = screen.getByTestId("signup-view")
+        expect(component).toBeTruthy()
+    })
+})
