ae7874cd9da19727302d0308d74215fae851d72f
Adrian L. Medeiros <lima.adrian13@gmail.com>
2024-08-28 12:48:55 +0000

    feat: add api proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import { fileURLToPath, URL } from 'node:url'
-
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    proxy:{
+      '/api': {
+        target: process.env.VITE_API_BASE_URL,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
